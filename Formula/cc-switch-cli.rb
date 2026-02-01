11fd189bbe125eee6e0c2f0a200de0d6053d7382
cc-switch-cli release <actions@github.com>
2026-02-01 05:18:40 +0000

    chore: update cc-switch-cli v0.1.0

--- Formula/cc-switch-cli.rb
+++ Formula/cc-switch-cli.rb
@@ -1,28 +1,28 @@
 class CcSwitchCli < Formula
   desc "Command-Line Management Tool for Claude Code, Codex & Gemini CLI"
   homepage "https://github.com/kingcanfish/cc-switch-cli"
-  version "0.0.6"
+  version "0.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/kingcanfish/cc-switch-cli/releases/download/v0.0.6/cc-switch-cli-v0.0.6-darwin-arm64.tar.gz"
-      sha256 "2f6d7bf2f5a91be99b0c12d73c2b9851c7cd302419345532ac575f292cbb69e0"
+      url "https://github.com/kingcanfish/cc-switch-cli/releases/download/v0.1.0/cc-switch-cli-v0.1.0-darwin-arm64.tar.gz"
+      sha256 "d00ad2e166b85080748d46e733fe80f6ed94826ac69acc177245f28cfc984045"
     end
     on_intel do
-      url "https://github.com/kingcanfish/cc-switch-cli/releases/download/v0.0.6/cc-switch-cli-v0.0.6-darwin-x64.tar.gz"
-      sha256 "2143381018e9fe21736ea8d975dc3d8cb85ce0ef73c284cdbd8f49aad8970def"
+      url "https://github.com/kingcanfish/cc-switch-cli/releases/download/v0.1.0/cc-switch-cli-v0.1.0-darwin-x64.tar.gz"
+      sha256 "6b6c302cce3e275f3cbfc0bbf174ba49fb5559d62906cdf6ba30b5e25e63ba66"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/kingcanfish/cc-switch-cli/releases/download/v0.0.6/cc-switch-cli-v0.0.6-linux-arm64-musl.tar.gz"
-      sha256 "7954d1c2629257f688b468ac67c201d5a4cb8593b544e9a765bef77f90d31d66"
+      url "https://github.com/kingcanfish/cc-switch-cli/releases/download/v0.1.0/cc-switch-cli-v0.1.0-linux-arm64-musl.tar.gz"
+      sha256 "bbb13bdea6d412580ff0aeb6e0a4d0df637b43dc0389f388249e38c9e2c2f5a3"
     end
     on_intel do
-      url "https://github.com/kingcanfish/cc-switch-cli/releases/download/v0.0.6/cc-switch-cli-v0.0.6-linux-x64-musl.tar.gz"
-      sha256 "8c1f7ce79a732ea15de47c802ed2bcbd66906f67e57c3463febb572623092c74"
+      url "https://github.com/kingcanfish/cc-switch-cli/releases/download/v0.1.0/cc-switch-cli-v0.1.0-linux-x64-musl.tar.gz"
+      sha256 "520b5a3d682678159b020ee8ed82edbc451465b927117df0790387d13bb80b47"
     end
   end
 
